132045e09a7bdd4a2069cee8bf4954bbd12441ad
Pratyush Rahul Ranjan <ranjan.pratyush24@gmail.com>
2025-11-16 17:03:20 +0530

    lol

--- C_Target_Practice.cpp
+++ C_Target_Practice.cpp
@@ -7,16 +7,9 @@ void solve(){
     for(int i=0; i<10; i++){
         for(int j=0; j<10; j++){
             cin >> grid[i][j];
-            if(grid[i][j]!='.' && (i==0 || j==0 || i==9 || j==9)){
-                count++;
-            }else if(grid[i][j]!='.' && (i==1 || j==1 || i==8 || j==8)){
-                count += 2;
-            }else if(grid[i][j]!='.' && (i==2 || j==2 || i==7 || j==7)){
-                count += 3;
-            }else if(grid[i][j]!='.' && (i==3 || j==3 || i==6 || j==6)){
-                count += 4;
-            }else if(grid[i][j]!='.' && (i==4 || j==4 || i==5 || j==5)){
-                count += 5;
+            if(grid[i][j]!='.'){
+                int point = min({i,j,9-i,9-j});
+                count += point+1;
             }
         }
     }
