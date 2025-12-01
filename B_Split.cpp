c72bb866cd1b3630a18146453e08da0e4954e026
Pratyush Rahul Ranjan <ranjan.pratyush24@gmail.com>
2025-12-01 11:35:04 +0530

    lol

--- B_Split.cpp
+++ B_Split.cpp
@@ -4,39 +4,34 @@ using namespace std;
 void solve(){
     int n;
     cin >> n;
-    vector<int>a(2*n);
-    map<int, int>mp;
-    for(int i=0; i<2*n; i++){
+    vector<int> a(2*n);
+    for(int i = 0;i < 2*n;i++){
         cin >> a[i];
-        mp[a[i]]++;
     }
 
-    int ans = 0;
-    int numOdd=0, numEven=0;
-    for(auto it : mp){
-        if(it.second %2 == 0){
-            numEven++;
-        }else{
-            numOdd++;
-        }
+    vector<int> cnt(2*n+1);
+    for(auto &x : a)cnt[x]++;
+
+    int x = 0,y = 0,z = 0;
+    for(int i = 1;i <= 2*n;i++){
+        if(cnt[i] == 0)continue; 
+        if(cnt[i]&1)x++;         //odd elements
+        else if(cnt[i]%4)y++;    //element of form 4*k + 2
+        else z++;                //element of form 4*k 
     }
 
-    if(numEven == mp.size()){
-        cout << 0 << endl;
-        return;
-    }
-
-    cout << "Working" << endl;
+    int ans = x+2*y+2*z;
+    if((z&1) && x == 0)ans -= 2; 
+    cout << ans << "\n";
 }
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     int t;
     cin >> t;
     while(t--){
         solve();
     }
-    return 0;
 }
